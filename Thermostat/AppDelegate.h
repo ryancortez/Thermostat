2e109c95058a1a905235cd2413da9c13f0898011
ryancortez <ryancortez@mac.com>
2016-06-16 11:50:20 -0500

    Initial Commit

---
+++ Thermostat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thermostat
+//
+//  Created by Ryan Cortez on 6/16/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thermostat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thermostat
+//
+//  Created by Ryan Cortez on 6/16/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
